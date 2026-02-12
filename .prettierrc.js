0b3408c6bae2c6a88cb453fd0c089e44dab8f22b
Inhoko <93079771+Inhoko@users.noreply.github.com>
2026-02-12 22:49:26 +0900

    Initialize project using Create React App

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBraketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+}
